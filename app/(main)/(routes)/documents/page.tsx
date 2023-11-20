da763dbac7f6556e0b50bd7daa7fd48b42e26f03
WalterIt <whiruba@gmail.com>
2023-11-20 05:07:38 -0300

    02:38:23 - Schema & API : Installed Sonner(toaster) and added it to layout of main Folder. CREATED: schema & documents to convex Folder.

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -4,9 +4,23 @@ import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { PlusCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 const DocumentsPage = () => {
     const { user } = useUser();
+    const create = useMutation(api.documents.create)
+    
+    const onCreate = () => {
+        const promise = create({ title: "Untitled" });
+
+        toast.promise(promise, {
+            loading: "Creating note...",
+            success: "Note created!",
+            error: "Failed to create note.",
+        })
+    }
 
     return ( 
         <div className="h-full flex flex-col items-center justify-center space-y-4 ">
@@ -15,7 +29,7 @@ const DocumentsPage = () => {
             <h2 className="text-lg font-medium">
                 Welcome to <b>{user?.username?.toUpperCase()}&apos;s</b> MNotion!
             </h2>
-            <Button>
+            <Button onClick={onCreate} >
                 <PlusCircle className="mr-2 h-4 w-4" />
                 Create a Note
             </Button>
