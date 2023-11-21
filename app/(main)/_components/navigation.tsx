c69a4d574b55a29303c4dd4036fa606b4b672bd4
WalterIt <whiruba@gmail.com>
2023-11-21 10:16:25 -0300

    03:58:12 - Trash box. 04:02:47: Implementations: fetch Archived documents, Restore & Remove Functions. Installed popover ui.

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
+import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings, Trash } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -11,6 +11,8 @@ import { api } from "@/convex/_generated/api";
 import Item from "./item";
 import { toast } from "sonner";
 import DocumentList from "./document-list";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+
 
 const Navigation = () => {
     const pathname = usePathname();
@@ -127,6 +129,14 @@ const Navigation = () => {
                 <div className="mt-4">
                    <DocumentList />
                     <Item onClick={handleCreate} label="Add a Page" icon={PlusCircle} />
+                    <Popover >
+                        <PopoverTrigger className="w-full mt-4">
+                            <Item label="Trash" icon={Trash}  />
+                        </PopoverTrigger>
+                        <PopoverContent side={isMobile ? "bottom" : "right"} className="p-0 w-72" >
+                            <p className="">Trash Box</p>
+                        </PopoverContent>                       
+                    </Popover>
                 </div> 
                 <div 
                     onMouseDown={handleMouseDown} 
