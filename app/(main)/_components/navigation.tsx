261b1c6045322abd547a9bd489fd4ab111047a07
WalterIt <whiruba@gmail.com>
2023-11-22 01:30:52 -0300

    04:11:50: Created Trash Box  Components. Installed: alert-dialog & input. 04:25:20: Creation of a Confirmation Modal to Delete Permanently a document.

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -12,6 +12,7 @@ import Item from "./item";
 import { toast } from "sonner";
 import DocumentList from "./document-list";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import TrashBox from "./trash-box";
 
 
 const Navigation = () => {
@@ -134,7 +135,7 @@ const Navigation = () => {
                             <Item label="Trash" icon={Trash}  />
                         </PopoverTrigger>
                         <PopoverContent side={isMobile ? "bottom" : "right"} className="p-0 w-72" >
-                            <p className="">Trash Box</p>
+                            <TrashBox />
                         </PopoverContent>                       
                     </Popover>
                 </div> 
