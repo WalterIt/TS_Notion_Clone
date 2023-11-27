c222074c324a258a10fa923db0808348127772a1
WalterIt <whiruba@gmail.com>
2023-11-27 11:06:56 -0300

    05:46:11 Implemented Toolbar component (Icon-picker & toolbar) on Page documentId. Installed emoji-picker-react

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,9 +1,33 @@
 'use client';
 
-const DocumentIdPage = () => {
+import { Toolbar } from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+
+interface DocumentIdPageProps {
+    params: {
+        documentId: Id<"documents">
+    }
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+    const document = useQuery(api.documents.getById, {documentId: params.documentId});
+
+    if (document === undefined) {
+        return (<div className="">Loading...</div>)
+    }
+
+    if (document === null) {
+        return (<div className="">Document not found!</div>)
+    }
+
     return ( 
-        <div className="">
-            Document Id Page.
+        <div className="pb-40">
+            <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+                <div className="h-[35vh] "  />
+                <Toolbar initialData={document} />
+            </div>
         </div>
      );
 }
