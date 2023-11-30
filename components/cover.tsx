3bc430131d356cf1225962e289982fddf364ba64
WalterIt <whiruba@gmail.com>
2023-11-30 10:51:26 -0300

    06:58:03 Created Editor component. Added an Extra layer of Security

--- components/cover.tsx
+++ components/cover.tsx
@@ -10,6 +10,7 @@ import { api } from "@/convex/_generated/api";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
 import { useEdgeStore } from "@/lib/edgestore";
+import { Skeleton } from "./ui/skeleton";
 
 
 interface CoverProps {
@@ -65,5 +66,11 @@ export const Cover = ({ url, preview }: CoverProps) => {
                 </div>
             )}
         </div>
+    )
+}
+
+Cover.Skeleton = function CoverSkeleton() {
+    return (
+        <Skeleton className="h-[12vh] w-full"  />
     )
 }
